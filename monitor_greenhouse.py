461927d9b86e7b0a935a47a87f14e6f896e5da36
Darin London <darin.london@duke.edu>
2021-01-28 20:53:43 -0500

    track metrics and report in prometheus format to file

---
+++ monitor_greenhouse.py
@@ -0,0 +1,31 @@
+from veml6030 import VEML6030
+import smbus2
+import qwiic_bme280
+import time
+import sys
+
+bus = smbus2.SMBus(1)  # For Raspberry Pi 
+light_sensor = VEML6030(bus)
+environment_sensor = qwiic_bme280.QwiicBme280()
+
+if environment_sensor.is_connected() == False:
+    print("The Environment Sensor isn't connected to the system. Please check your connection", file=sys.stderr)
+    exit(1)
+
+environment_sensor.begin()
+
+def instrument_metrics(temp, light):
+  metrics_out = open('/home/pi/metrics', 'w+')
+  print('# HELP ambient_temperature temperature in fahrenheit', flush=True, file=metrics_out)
+  print('# TYPE ambient_temperature gauge', flush=True, file=metrics_out)
+  print(f'ambient_temperature {temp}', flush=True, file=metrics_out)
+  print('# HELP ambient_light light in lux', flush=True, file=metrics_out)
+  print('# TYPE ambient_light gauge', flush=True, file=metrics_out)
+  print(f'ambient_light {light}', flush=True, file=metrics_out)
+  metrics_out.close()
+
+while True:
+    light = light_sensor.read_light()
+    temp = environment_sensor.temperature_fahrenheit
+    instrument_metrics(light, temp)
+    time.sleep(11)
